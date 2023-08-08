c466ae7cda79e67392827b145b25ef1caf251de8
Ido Eitach <112800537+IdoEitach@users.noreply.github.com>
2023-08-08 17:23:56 +0300

    adding Next to sign up

--- src/app/pages/signUpPage/UserName.tsx
+++ src/app/pages/signUpPage/UserName.tsx
@@ -3,11 +3,20 @@ import { useState, useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import { TextField } from "@mui/material";
 import BaseSignUp from "@/app/components/baseSingUp";
+import Link from "next/link";
+import { Button } from "@mui/material";
 
 function UserName() {
   return (
     <div>
-      <BaseSignUp numberOFFields={3} label={["userName", "password"]} />
+      <div>
+        <BaseSignUp numberOFFields={3} label={["userName", "password"]} />
+      </div>
+      <div>
+        <Button variant="contained" size="large">
+          Next
+        </Button>
+      </div>
     </div>
   );
 }

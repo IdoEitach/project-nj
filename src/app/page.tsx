eca541be85750461fef37a272318206408ff4361
Ido Eitach <112800537+IdoEitach@users.noreply.github.com>
2023-08-08 10:28:14 +0300

    fixing bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
+import HomePage from "./pages/homePage/homePage";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <HomePage />
     </div>
   );
 }
